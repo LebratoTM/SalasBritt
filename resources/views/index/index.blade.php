03eed9ac3ce6a1796c0473d3f4f1ca7ab462edbd
LebratoTM <albertomagana001@gmail.com>
2024-08-17 19:45:55 -0600

    Agregando el proyecto

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de sesion</title>
+    <link rel="stylesheet" href="index/styles.css">
+    <link href="index/script.js">
+</head>
+<body>
+    <div class="nav-bar">
+        <div class="name">
+            <p>Salas Brit</p>
+        </div>
+    </div>
+    <div class="body-container">
+        <h1>Quienes somos</h1>
+        <p>Somos fabricantes de todo tipo de muebles para el hogar,</p>
+        <p>desde salas completas, reposet, sofacamas,</p>
+        <p>en distintos tamaños, desde el tamaño estandar, queen size, king size, etc</p>
+
+        <p>Visita nuestra tienda en linea, y adquiere </p>
+    </div>
+</body>
+<style>
+    *{
+        margin: 0;
+        background: url('background.jpg');
+        justify-content: center;
+    }
+
+    .nav-bar{
+        padding: 10px;
+        background-color: #564701;
+        font-family: Arial, sans serif;
+        font-size: 10px;
+        justify-content: center;
+        color: white;
+    }
+    
+    .body-container{
+        /* display: flex; */
+        font-family: Arial, sans serif;
+        align-items: center;
+        justify-content: center;
+        background-color: red;
+        height: 300px;
+        width: 400px;
+    }
+</style>
+</html>
